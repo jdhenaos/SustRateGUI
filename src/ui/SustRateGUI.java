779fba08afbf106f432aebbdb59c60982310c5fb
juancholkovich <judhenaosa@unal.edu.co>
2018-01-18 08:46:43 -0500

    window backbone

--- src/ui/SustRateGUI.java
+++ src/ui/SustRateGUI.java
@@ -1,13 +1,92 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+
 public class SustRateGUI extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	private Border border;
+
+	public SustRateGUI(){
+		
+		super("SustRate");
+		
+		Container main = getContentPane();
+		main.setLayout(new BorderLayout());
+		
+		border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		
+		setSize(500,500);
+		setVisible(true);
+		
+		this.addWindowListener(new Close());
+		
+	}
+	
+	
+	class Close implements WindowListener{
+
+		@Override
+		public void windowOpened(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowClosing(WindowEvent e) {
+			// TODO Auto-generated method stub
+			setVisible(false);
+			dispose();
+			System.exit(0);
+		}
+
+		@Override
+		public void windowClosed(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowIconified(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowActivated(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new x
+		
+		new SustRateGUI();
 
 	}
 
