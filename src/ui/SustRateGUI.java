3c443e48185579066451c28c86a22058453da89f
juancholkovich <judhenaosa@unal.edu.co>
2018-01-18 09:31:26 -0500

    window backbone

--- src/ui/SustRateGUI.java
+++ src/ui/SustRateGUI.java
@@ -6,14 +6,24 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class SustRateGUI extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
 	private Border border;
+	private JButton bOpen, bSave, bCalculate;
+	private DefaultTableModel dtShow;
+	private JTable table;
+	
 
 	public SustRateGUI(){
 		
@@ -24,11 +34,35 @@ public class SustRateGUI extends JFrame{
 		
 		border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
 		
+		JPanel middle = Center();
+		
+		main.add(middle,BorderLayout.CENTER);
+		
 		setSize(500,500);
 		setVisible(true);
 		
 		this.addWindowListener(new Close());
 		
+	}
+	
+	public JPanel Center(){
+		
+		JPanel center = new JPanel(new BorderLayout());
+		
+		TitledBorder title = BorderFactory.createTitledBorder(border,"Result");
+		center.setBorder(title);
+		
+		table = new JTable();
+		dtShow = (DefaultTableModel) table.getModel();
+		
+		JScrollPane scroll = new JScrollPane(table);
+		
+		dtShow.addColumn("First ID");
+		dtShow.addColumn("Second ID");
+		
+		center.add(scroll,BorderLayout.CENTER);
+		
+		return center;
 	}
 	
 	
