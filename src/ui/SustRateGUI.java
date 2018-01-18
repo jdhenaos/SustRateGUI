915a4302d43d5bb005bc1a938cd071c1acf9e5b9
juancholkovich <judhenaosa@unal.edu.co>
2018-01-18 08:07:41 -0500

    init code

---
+++ src/ui/SustRateGUI.java
@@ -0,0 +1,14 @@
+package ui;
+
+public class SustRateGUI extends JFrame{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new x
+
+	}
+
+}
